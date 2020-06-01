913d8d861750d0ea9c87c6de654ea649d7d8e0c8
lys <18684824230@163.com>
2020-06-01 12:08:14 +0800

    搭建小程序环境

---
+++ bill-app/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
